f0a9072c32ac86d3ced9de6574481e1b01402987
i343811 <emoin.lanyu@sap.com>
2022-02-28 11:11:18 +0800

    Enable default server side encryption for s3 garden etcd bucket

--- components/etcd/backupinfra/provider/s3/main.tf
+++ components/etcd/backupinfra/provider/s3/main.tf
@@ -30,6 +30,14 @@ resource "aws_s3_bucket" "bucket" {
   tags = {
     Name = var.LANDSCAPE
   }
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 //=====================================================================
